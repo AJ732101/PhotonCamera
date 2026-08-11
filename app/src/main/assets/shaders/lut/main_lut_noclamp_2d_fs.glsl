e6c546d2ce3458fbbebfa6120a482105f76e1ca1
AJ732101 <aj732101@gmail.com>
2026-08-11 13:08:42 +0200

    Added missing shader

---
+++ app/src/main/assets/shaders/lut/main_lut_noclamp_2d_fs.glsl
@@ -0,0 +1,45 @@
+#version 300 es
+precision highp float;
+
+uniform sampler2D sTexture;
+uniform sampler2D PostLut;
+uniform float     POSTLUTSIZE;
+uniform float     POSTLUTSIZETILES;
+
+in vec2 v_texCoord;
+out vec4 Output;
+
+vec3 applyLut(in vec3 textureColor) {
+    highp vec3 clampedColor = clamp(textureColor, 0.0, 1.0);
+    highp float blueColor = clampedColor.b * (POSTLUTSIZE - 1.0);
+    highp float slice0_index = floor(blueColor);
+    highp float slice1_index = ceil(blueColor);
+
+    highp vec2 slice0_pos;
+    slice0_pos.y = floor(slice0_index / POSTLUTSIZETILES);
+    slice0_pos.x = slice0_index - (slice0_pos.y * POSTLUTSIZETILES);
+
+    highp vec2 slice1_pos;
+    slice1_pos.y = floor(slice1_index / POSTLUTSIZETILES);
+    slice1_pos.x = slice1_index - (slice1_pos.y * POSTLUTSIZETILES);
+
+    highp float tileWidth = 1.0 / POSTLUTSIZETILES;
+    highp vec2 rg_offset = clampedColor.rg * tileWidth;
+
+    highp vec2 slice0_start_uv = slice0_pos * tileWidth;
+    highp vec2 slice1_start_uv = slice1_pos * tileWidth;
+
+    highp vec2 texPos1 = slice0_start_uv + rg_offset;
+    highp vec2 texPos2 = slice1_start_uv + rg_offset;
+
+    highp vec3 newColor1 = texture(PostLut, texPos1).rgb;
+    highp vec3 newColor2 = texture(PostLut, texPos2).rgb;
+
+    return mix(newColor1, newColor2, fract(blueColor));
+}
+
+void main() {
+    vec3 color = texture(sTexture, v_texCoord).rgb;
+    vec3 finalColor = applyLut(color);
+    Output = vec4(finalColor, 1.0);
+}
